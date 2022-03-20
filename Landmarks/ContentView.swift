f73816e781913abed0ecd99439a18a835058769c
floydkim <for12gm@gmail.com>
2022-03-20 21:38:27 +0900

    ~ section 2

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -7,13 +7,18 @@
 
 import SwiftUI
 
+// The first structure conforms to the View protocol and describes the view’s content and layout.
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        // Each modifier returns a new view, so it’s common to chain multiple modifiers, stacked vertically.
+        Text("Turtle Rock")
+            .font(.title)
             .padding()
+            .foregroundColor(.black)
     }
 }
 
+// The second structure declares a preview for that view.
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -7,8 +7,9 @@
 
 import SwiftUI
 
-@main
-struct LandmarksApp: App {
+@main // The @main attribute identifies the app’s entry point.
+struct LandmarksApp: App { // An app that uses the SwiftUI app life cycle has a structure that conforms to the App protocol.
+    // The structure’s body property returns one or more scenes.
     var body: some Scene {
         WindowGroup {
             ContentView()
