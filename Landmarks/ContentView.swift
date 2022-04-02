7639c2fefc3d623ca524d4b792ede6341ac680fa
floydkim <for12gm@gmail.com>
2022-04-02 21:11:31 +0900

    'Creating and Combining Views' done

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,19 +10,39 @@ import SwiftUI
 // The first structure conforms to the View protocol and describes the view’s content and layout.
 struct ContentView: View {
     var body: some View {
-        // Each modifier returns a new view, so it’s common to chain multiple modifiers, stacked vertically.
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            // When you specify only the height parameter, the view automatically sizes to the width of its content.
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            // Each modifier returns a new view, so it’s common to chain multiple modifiers, stacked vertically.
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+            
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Tertle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
