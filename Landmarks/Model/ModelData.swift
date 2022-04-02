55a6cb947b3513e3196d85858dcbbbda948f475c
floydkim <for12gm@gmail.com>
2022-04-02 21:35:22 +0900

    Building Lists and Navigation > section 1 'Create a Landmark Model' done

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,34 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by 김민창 on 2022/04/02.
+//
+
+import Foundation
+
+// array of landmarks that you initialize from landmarkData.json.
+var landmarks: [Landmark] = load("landmarkData.json")
+
+// fetches JSON data with a given name from the app’s main bundle.
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
