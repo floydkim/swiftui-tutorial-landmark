03c969b0526daf850dcfe0687129c180577a30ce
floydkim <for12gm@gmail.com>
2022-04-03 16:35:07 +0900

    'Handling User Input' > section 1 'Mark the User's Favorite Landmarks' done

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -37,7 +37,7 @@ struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         // ForEach operates on collections the same way as the list, which means you can use it anywhere you can use a child view, such as in stacks, lists, groups, and more.
         // When the elements of your data are simple value types — like the strings you’re using here — you can use \.self as key path to the identifier.
-        ForEach(["iPhone 13 Pro", "iPhone 8"], id: \.self) { deviceName in
+        ForEach(["iPhone 13 Pro"/*, "iPhone 8"*/], id: \.self) { deviceName in
             LandmarkList()
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
