740ac7dca286f27b467eb235f18ce421cef5aabe
floydkim <for12gm@gmail.com>
2022-04-03 15:54:34 +0900

    'Building Lists and Navigation' DONE > section 8 'Generate Previews Dynamically' done

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -35,6 +35,12 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        // ForEach operates on collections the same way as the list, which means you can use it anywhere you can use a child view, such as in stacks, lists, groups, and more.
+        // When the elements of your data are simple value types — like the strings you’re using here — you can use \.self as key path to the identifier.
+        ForEach(["iPhone 13 Pro", "iPhone 8"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
